c77167a1f8ccb1b801f04742d066d355400cfe4e
webbertsai <begining1003@gmail.com>
2013-01-30 01:36:04 +0800

    新增 post/comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
-
+  has_many :comment
 end
